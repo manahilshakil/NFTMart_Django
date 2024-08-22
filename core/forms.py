1b5c2493f32a15c05f922fb2cb70d1c7435290c0
manahilshakil <i.manahilshakil@gmail.com>
2024-08-22 02:18:36 -0700

    made signup form, html and views and signup method

--- core/forms.py
+++ core/forms.py
@@ -7,3 +7,22 @@ class SignupForm(UserCreationForm):
         model = User
         fields = ('username', 'email','password1','password2')
         
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder' : 'Your username',
+        'class' : 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder' : 'Email address',
+        'class' : 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : 'Enter password',
+        'class' : 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : 'Confirm password',
+        'class' : 'w-full py-4 px-6 rounded-xl'
+    }))
