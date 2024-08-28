7d28a08b326daf6cc309aed8099ca365333304a6
manahilshakil <i.manahilshakil@gmail.com>
2024-08-28 06:33:01 -0700

    created edit item views,detail.html

--- item/forms.py
+++ item/forms.py
@@ -26,3 +26,29 @@ class NewItemForm(forms.ModelForm):
         }),
 
     }
+        
+from django import forms
+
+from .models import Item
+
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name','description','price','image','is_sold',)
+        widgets = {
+        'name': forms.TextInput(attrs={
+            'class' : INPUT_CLASSES
+        }),
+        'description': forms.Textarea(attrs={
+            'class' : INPUT_CLASSES
+        }),
+        'price': forms.TextInput(attrs={
+            'class' : INPUT_CLASSES
+        }),
+        'image': forms.FileInput(attrs={
+            'class' : INPUT_CLASSES
+        }),
+
+    }
--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,get_object_or_404, redirect
 from .models import Item
-from .forms import NewItemForm
+from .forms import EditItemForm, NewItemForm
 
 # Create your views here.
 def detail(request, pk):
@@ -25,11 +25,31 @@ def new(request):
             item.save()
 
             return redirect('item:detail', pk=item.id)
-    form = NewItemForm()
+    else:
+        form = NewItemForm()
 
     return render(request, 'item/form.html',{
         'form' : form,
         'title' : 'New item'
+    })
+
+@login_required
+def edit(request,pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST,request.FILES,instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html',{
+        'form' : form,
+        'title' : 'Edit item'
     })
 
 @login_required
