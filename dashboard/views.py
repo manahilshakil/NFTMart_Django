64495a46563ee4748a6ca0cdd5e50d239f40f720
manahilshakil <i.manahilshakil@gmail.com>
2024-08-28 02:56:29 -0700

    created delete item views,detail.html

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 # Create your views here.
 from item.models import Item
